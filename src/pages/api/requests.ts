ae0b203b1bb764646bb685ebbb37febaa9a1c6dc
Ylan <hei.nalisoa@gmail.com>
2023-05-27 19:44:09 +0300

    feat: type user

---
+++ src/pages/api/requests.ts
@@ -0,0 +1,14 @@
+export type User = {
+    email: String;
+    password: String;
+    name: String;
+    googleId: Number;
+    bio: String;
+    status: String;
+}
+
+export type UserLogin = {
+    email: String;
+    password: String;
+    name: String;
+}
