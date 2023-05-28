dd9113dc4dc366be59f22678f6f5f9ab38f2bf63
Ylan <hei.nalisoa@gmail.com>
2023-05-28 16:31:20 +0300

    refactor: remove navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,10 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   
   return (
     <div>
-
-      <Navbar/>
-      <Component {...pageProps} />
-    
+      <Component {...pageProps} />    
     </div>
   )
 }
