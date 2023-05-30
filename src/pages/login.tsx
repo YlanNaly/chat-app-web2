2231b0b9088baab17e0b7e7d991cdd2bfa96c098
Ylan <hei.nalisoa@gmail.com>
2023-05-30 16:47:46 +0300

    fix: login and signup register

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,11 +7,46 @@ import { UserLogin } from "@/pages/api/requests";
 import Authentication from "@/components/organisms/authentication";
 
 const Login =()=>{
+  const router = useRouter();
+  
+  const onSubmit: SubmitHandler<UserLogin> = data => {
+    axios.post<UserLogin>(
+    `${BASE_URL}/users/login`
+    ,{
+      headers:{
+          authorization: process.env.NEXT_PUBLIC_JWT_TOKEN,
+          'Content-Type': "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => {
+      if(res.status == 201 || res.status == 200) {
 
+        localStorage.setItem('nom', data.name.toString());
+        localStorage.setItem('email', data.email.toString());
+        localStorage.setItem('motDePasse', data.password.toString());
+
+        router.push("/channel/channel")
+        return;
+      }
+      router.push(`login`);
+    })
+    .catch(er => console.log(er))
+  }
+  useEffect(() => {
+    const storedNom = localStorage.getItem('nom');
+    const storedEmail = localStorage.getItem('email');
+    const storedMotDePasse = localStorage.getItem('motDePasse');
+
+    if (storedNom && storedEmail && storedMotDePasse) {
+      router.push('/channel/channel');
+    }
+  }, [router]);
   return(
     <Authentication 
         title={"Authentification"} 
         isLogin={true} 
+        handlerSubmit={onSubmit}
     />
   )
 }
